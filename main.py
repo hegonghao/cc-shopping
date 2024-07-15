077689d33e4f38829a9f333413c0cb2e5f3db623
hegonghao <hegonghao1@163.com>
2024-07-15 18:20:51 +0800

    Removed redundant add_two_numbers function from main.py

--- main.py
+++ main.py
@@ -4,23 +4,8 @@ from interface import RushGUI
 from log import logger
 
 
-def add_two_numbers(a, b):
-    """
-    Calculate and return the sum of two numbers.
-    
-    Args:
-    a (int or float): The first number to add
-    b (int or float): The second number to add
-    
-    Returns:
-    int or float: The sum of a and b
-    """
-    return a + b
-
-
 if __name__ == '__main__':
     #test
     global_config = Config()
     chenchao = JdUser(global_config)
     chenchao_gui = RushGUI(chenchao)
-
